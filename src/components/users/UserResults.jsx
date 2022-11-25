1a33b23a6f19fd9d1fbecce4f27080fe70a50d44
Lady Liliana Vasquez Saiz <lady.vasquez@globant.com>
2022-11-25 16:25:39 -0500

    Add global github context

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,33 +1,19 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect, useContext } from 'react';
 
 /* Components */
 import Spinner from '../layout/Spinner';
 import UserItem from './UserItem';
 
+/* Others */
+import GithubContext from '../../context/github/GithubContext';
+
 const UserResults = () => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const { users, loading, fetchUsers } = useContext(GithubContext);
 
   useEffect(() => {
     fetchUsers();
   }, []);
-
-  /* The token was giving an error */
-  const fetchUsers = async () => {
-    const response = await fetch(
-      `${process.env.REACT_APP_GITHUB_URL}/users` /*  {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    } */
-    );
-
-    const data = await response.json();
-
-    setUsers(data);
-    setLoading(false);
-  };
 
   if (!loading) {
     return (
