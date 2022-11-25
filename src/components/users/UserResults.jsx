87e25bd464d99f62450ebddea68589ce9511cf4a
Lady Liliana Vasquez Saiz <lady.vasquez@globant.com>
2022-11-25 14:29:44 -0500

    Add userResults and spinner components

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 
+/* Components */
+import Spinner from '../layout/Spinner';
+
 const UserResults = () => {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
   const fetchUsers = async () => {
     const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
@@ -18,20 +25,16 @@ const UserResults = () => {
     setLoading(false);
   };
 
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
   if (!loading) {
     return (
       <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
-        {users.map((user) => (
-          <h3>{user.login}</h3>
+        {users.map((user, id) => (
+          <h3 key={id}>{user.login}</h3>
         ))}
       </div>
     );
   } else {
-    return <h3>Loading...</h3>;
+    return <Spinner />;
   }
 };
 
